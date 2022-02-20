b2bd3042c0aa5a5bba5f3a4bb583378dd8db8bab
Tiny <405401372@gapp.fju.edu.tw>
2022-02-20 13:10:26 +0800

    add api/usernames

---
+++ pages/api/usernames.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    usernames: [
+      'TinyWugaga',
+      'GitHub',
+      'Vercel',
+      'Octokit',
+      'Notion'
+    ]
+  })
+}
