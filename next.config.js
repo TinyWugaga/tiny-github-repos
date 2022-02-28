eac954e7ffe5d1b1028ec9cc97639c6654fc269e
Tiny <405401372@gapp.fju.edu.tw>
2022-02-28 20:11:00 +0800

    add package & setting config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-const debug = process.env.NODE_ENV !== 'production'
-const vercelProd = process.env.DEPLOY_ENV === 'vercel'
+const debug = process.env.NODE_ENV !== 'production';
+const vercelProd = process.env.DEPLOY_ENV === 'vercel';
 
 module.exports = {
   reactStrictMode: true,
   assetPrefix: !(debug || vercelProd) ? '/tiny-github-page/' : '',
-}
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true
+  }
+};
