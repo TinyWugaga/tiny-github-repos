e7d4cf6f0d51fade882103228623a1d9f7fda6bd
Tiny <405401372@gapp.fju.edu.tw>
2022-03-28 15:33:04 +0800

    modify configs

--- env.config.js
+++ env.config.js
@@ -1,6 +1,10 @@
 const prod = process.env.NODE_ENV === 'production'
 const vercelProd = process.env.DEPLOY_ENV === 'vercel'
 
+const localhost = 'http://localhost:3000/'
+const apiDomain = prod ? (!vercelProd ? '/tiny-github-page/' : '') : localhost
+
 module.exports = {
-  'process.env.BACKEND_URL': (prod && !vercelProd) ? '/tiny-github-page' : '',
+  LOCAL_DOMAIN: localhost,
+  BACKEND_DOMAIN: apiDomain
 }
--- next.config.js
+++ next.config.js
@@ -1,20 +1,27 @@
-const debug = process.env.NODE_ENV !== 'production';
-const vercelProd = process.env.DEPLOY_ENV === 'vercel';
+const env = require('./env.config')
 
-module.exports = {
-  reactStrictMode: true,
-  assetPrefix: !(debug || vercelProd) ? '/tiny-github-page/' : '',
-  compiler: {
-    // ssr and displayName are configured by default
-    styledComponents: true
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/users',
-        permanent: false,
-      },
-    ]
+module.exports = (phase) => {
+  return {
+    env,
+    reactStrictMode: true,
+    assetPrefix: env.BACKEND_DOMAIN,
+    compiler: {
+      // ssr and displayName are configured by default
+      styledComponents: true
+    },
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/users',
+          permanent: true,
+        },
+        {
+          source: '/error',
+          destination: '/404',
+          permanent: true,
+        },
+      ]
+    }
   }
 };
