1b75b267f83d6f074a533f6dd1288f205134178a
Tiny <405401372@gapp.fju.edu.tw>
2022-03-28 15:32:55 +0800

    modify global styles

--- styles/global.js
+++ styles/global.js
@@ -38,6 +38,18 @@ export default createGlobalStyle`
     margin-bottom: 0;
   }
 
+  h1 {
+    font-size: 32px;
+    font-weight: 600;
+
+    margin: 0.67em 0;
+  }
+
+  h2 {
+    font-size: 24px;
+    font-weight: 600;
+  }
+
   h3 {
     font-size: 20px;
     font-weight: 600;
