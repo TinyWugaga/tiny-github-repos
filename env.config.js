fcda1dac1712d70d55132683a8187b6250060097
Tiny <405401372@gapp.fju.edu.tw>
2022-04-04 02:14:43 +0800

    fix env domain

--- env.config.js
+++ env.config.js
@@ -1,8 +1,7 @@
 const prod = process.env.NODE_ENV === 'production'
-const vercelProd = process.env.DEPLOY_ENV === 'vercel'
 
 const localhost = 'http://localhost:3000/'
-const apiDomain = prod ? (!vercelProd ? '/tiny-github-page/' : '') : localhost
+const apiDomain = prod ? process.env.DEPLOY_DOMAIN : localhost
 
 const tinyPage ='https://tiny-box.notion.site/Hello-This-is-Tiny-Box-cd0c13d727ec4c7197d91b7346ac57ab'
 
