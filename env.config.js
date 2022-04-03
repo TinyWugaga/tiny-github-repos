ac35f439dfcdf4d9c9fd6edf6ec2cab8f86ba6a6
Tiny <405401372@gapp.fju.edu.tw>
2022-04-03 14:52:13 +0800

    add GA

--- env.config.js
+++ env.config.js
@@ -5,11 +5,9 @@ const localhost = 'http://localhost:3000/'
 const apiDomain = prod ? (!vercelProd ? '/tiny-github-page/' : '') : localhost
 
 const tinyPage ='https://tiny-box.notion.site/Hello-This-is-Tiny-Box-cd0c13d727ec4c7197d91b7346ac57ab'
-const githubToken = process.env.GITHUB_TOKEN
 
 module.exports = {
   LOCAL_DOMAIN: localhost,
   BACKEND_DOMAIN: apiDomain,
-  GITHUB_TOKEN: githubToken,
   TINY_PAGE: tinyPage
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,18 @@
-import Document from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -21,10 +20,41 @@ export default class MyDocument extends Document {
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        ),
-      }
+        )
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                  page_path: window.location.pathname
+                  ${(process.env.NODE_ENV === 'development' && ",debug_event: 1,traffic_type : 'internal'") || ''}
+                });
+              `
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
